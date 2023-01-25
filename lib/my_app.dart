0dd37c75119e933502a8fe3778a68fa9c255c0ef
Sheldon <tinarychina@gmail.com>
2023-01-25 21:14:58 +0800

    添加隐私保护功能

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -2,7 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:ty_flutter_demo/xiaohongshu/pages/navigations/bottom_navbar.dart';
 // import './navigation/bottom_navigation_bar.dart';
 
-class MyAppDemo extends StatelessWidget {
+class MyAppDemo extends StatefulWidget {
+  @override
+  State<MyAppDemo> createState() => _MyAppDemoState();
+}
+
+class _MyAppDemoState extends State<MyAppDemo> with WidgetsBindingObserver {
+  // 控制隐私保护页面是否显示
+  bool _isHidden = false;
+  @override
+  void initState() {
+    super.initState();
+    // 将当前实例的监听添加到WidgetsBinding的实例对象中
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // 从 WidgetsBinding 的实例对象中移除对当前对象的监听
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    setState(() {
+      _isHidden = state != AppLifecycleState.resumed;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +43,15 @@ class MyAppDemo extends StatelessWidget {
       ),
       // home: HomePage(),
       // home: TYBottomNavBar(),
-      home: XHSBottomNavBar(),
+      home: Stack(
+        children: [
+          XHSBottomNavBar(),
+          if (_isHidden)
+            Container(
+              color: Colors.orange,
+            ),
+        ],
+      ),
     );
   }
 }
