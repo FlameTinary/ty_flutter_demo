2f48bb01e1697c41b06a4916d1ba4e4acead5b2c
Sheldon <tinarychina@gmail.com>
2023-01-29 22:43:58 +0800

    添加chatGPT聊天功能

---
+++ lib/xiaohongshu/models/message/open_model.dart
@@ -0,0 +1,97 @@
+// To parse this JSON data, do
+//
+//     final openModel = openModelFromJson(jsonString);
+
+import 'dart:convert';
+
+OpenModel openModelFromJson(String str) => OpenModel.fromJson(json.decode(str));
+
+String openModelToJson(OpenModel data) => json.encode(data.toJson());
+
+class OpenModel {
+    OpenModel({
+        required this.id,
+        required this.object,
+        required this.created,
+        required this.model,
+        required this.choices,
+        required this.usage,
+    });
+
+    String id;
+    String object;
+    int created;
+    String model;
+    List<Choice> choices;
+    Usage usage;
+
+    factory OpenModel.fromJson(Map<String, dynamic> json) => OpenModel(
+        id: json["id"],
+        object: json["object"],
+        created: json["created"],
+        model: json["model"],
+        choices: List<Choice>.from(json["choices"].map((x) => Choice.fromJson(x))),
+        usage: Usage.fromJson(json["usage"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "object": object,
+        "created": created,
+        "model": model,
+        "choices": List<dynamic>.from(choices.map((x) => x.toJson())),
+        "usage": usage.toJson(),
+    };
+}
+
+class Choice {
+    Choice({
+        required this.text,
+        required this.index,
+        this.logprobs,
+        required this.finishReason,
+    });
+
+    String text;
+    int index;
+    dynamic logprobs;
+    String finishReason;
+
+    factory Choice.fromJson(Map<String, dynamic> json) => Choice(
+        text: json["text"],
+        index: json["index"],
+        logprobs: json["logprobs"],
+        finishReason: json["finish_reason"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "text": text,
+        "index": index,
+        "logprobs": logprobs,
+        "finish_reason": finishReason,
+    };
+}
+
+class Usage {
+    Usage({
+        required this.promptTokens,
+        required this.completionTokens,
+        required this.totalTokens,
+    });
+
+    int promptTokens;
+    int completionTokens;
+    int totalTokens;
+
+    factory Usage.fromJson(Map<String, dynamic> json) => Usage(
+        promptTokens: json["prompt_tokens"],
+        completionTokens: json["completion_tokens"],
+        totalTokens: json["total_tokens"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "prompt_tokens": promptTokens,
+        "completion_tokens": completionTokens,
+        "total_tokens": totalTokens,
+    };
+}
--- lib/xiaohongshu/pages/messages/message_page.dart
+++ lib/xiaohongshu/pages/messages/message_page.dart
@@ -1,14 +1,75 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:ty_flutter_demo/xiaohongshu/models/message/open_model.dart';
 
-class XHSMessagePage extends StatelessWidget {
+class XHSMessagePage extends StatefulWidget {
   const XHSMessagePage({super.key});
+
+  @override
+  State<XHSMessagePage> createState() => _XHSMessagePageState();
+}
+
+class _XHSMessagePageState extends State<XHSMessagePage> {
+  String text = '';
+  String completion = '';
+
+  _getMessage() async {
+    Response? response;
+    var dio = Dio();
+    dio.options.baseUrl = 'https://api.openai.com/v1';
+    dio.options.headers = {
+      "Content-Type": " application/json",
+      "Accept-Encoding": "gzip",
+      "Transfer-Encoding": "chunked",
+      "Authorization":
+          "",
+    };
+    // 发起网络请求
+    try {
+      response = await dio.post('/completions', data: {
+        "model": "text-davinci-003",
+        "prompt": text,
+        "max_tokens": 2048,
+        "temperature": 0,
+        "top_p": 1,
+        "frequency_penalty": 0,
+        "presence_penalty": 0.6,
+        "stop": [" Human:", " AI:"]
+      });
+    } on DioError catch (e) {
+      print(e.message);
+    }
+
+    // print(response.data.toString());
+    OpenModel om = OpenModel.fromJson(response?.data);
+    print(om.toJson());
+    setState(() {
+      completion = om.choices.first.text;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('消息')),
       body: Center(
-        child: Text('message'),
+        child: Column(
+          children: [
+            TextField(
+              onChanged: (value) {
+                text = value;
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _getMessage();
+              },
+              child: Text('发送'),
+            ),
+            if (completion.length > 0) Text(completion),
+          ],
+        ),
       ),
     );
   }
